26d9a519f23c3a0f75ee64089ffcd906c1bcc34c
Albert Lay <albertjlay@gmail.com>
2022-09-09 18:57:47 -0400

    Add Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { LightTheme } from './Themes';
 import Nav from './components/sections/Nav';
 import Home from './components/sections/Home';
@@ -9,8 +9,14 @@ import Experience from './components/sections/Experience';
 import Projects from './components/sections/Projects';
 import Footer from './components/sections/Footer';
 import CallToAction from './components/sections/CallToAction';
+import ReactGA from 'react-ga';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('UA-204749576-2');
+    ReactGA.pageview('/');
+  }, []);
+
   return (
     <ThemeProvider theme={LightTheme}>
       <div className='App'>
