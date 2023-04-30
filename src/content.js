9ccdb03fef8d1e22600140b789edcaf54312339b
Albert Lay <albertjlay@gmail.com>
2023-04-30 08:25:07 +0800

    Update descriptions and about me

--- src/content.js
+++ src/content.js
@@ -27,19 +27,19 @@ import { ProjectType } from './types';
 export const aboutContent = [
   {
     header: 'I code.',
-    body: "I believe in the power of quality code, thorough documentation, and rigorous testing. With these pillars as my foundation, I have built modular, component-based frontend softwares with React, Vue, and TypeScript. I also have experience designing robust backend systems using Go, C#, and .NET. I'm always looking for a cool, new technology to try out!",
+    body: 'I believe in the power of quality code, thorough documentation, and rigorous testing. With these pillars as my foundation, I have built accessible, intuitive frontend applications with React, Vue, and TypeScript, as well as designed robust backend endpoints using Django, Go, and .NET. I’m always looking for a cool, new technology to try out!',
     image: laptopMemoji,
     alt: 'Memoji of me working on my laptop.',
   },
   {
     header: 'I (try to) have fun!',
-    body: 'While I *love* my work, it is not my life. Work-life balance is extremely important to me. On my free time, I scour the city for good food, enjoy long chats with good buddies, and lose in both League & Smash. I also love trying out new activities! Some of my newly discovered hobbies include skiing, boxing, and blogging.',
+    body: 'While I *love* my work, it is not my life. Work-life balance is extremely important to me. On my free time, I scour the city for good coffee, enjoy long chats with cool people, and try not to embarass myself while dancing. I also love trying out new activities! Some of my newly discovered hobbies include skiing, boxing, and biking.',
     image: partyMemoji,
     alt: 'Memoji of me partying.',
   },
   {
     header: "I'm not sure really...",
-    body: "While I'd like to pretend that I have everything figured out, I really don't. I'm still halfway through my bachelor's degree and I'm taking this time to explore all facets of software development & beyond. This includes system design, data science, and product management. It's been extremely fun exploring these multiple facets of computer science!",
+    body: "While I'd like to pretend that I have everything figured out, I really don't. I'm still halfway through my bachelor's degree and I'm taking this time to explore all facets of software development & beyond. This includes system design, artificial intelligence, and data science. I plan to explore the field as much as I can. Not settling down just yet!",
     image: shrugMemoji,
     alt: 'Memoji of me shrugging.',
   },
