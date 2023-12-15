1faa548ceb0ce4fdfb80557962c1d44fdf18fc4f
Albert Lay <albertjlay@gmail.com>
2023-12-15 10:33:19 +0100

    Add swipeable views to experience

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -5,6 +5,7 @@ import { Container } from '@mui/system';
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
 import { experienceContent } from '../../content';
 import { Fade } from 'react-awesome-reveal';
+import SwipeableViews from 'react-swipeable-views-react-18-fix';
 
 // https://stackoverflow.com/questions/50890241/programmatically-add-opacity-to-a-color-in-typescript
 function addAlpha(color, opacity) {
@@ -14,12 +15,21 @@ function addAlpha(color, opacity) {
 }
 
 const Experience = () => {
+  const [index, setIndex] = React.useState(0);
   const [viewedExp, setViewedExp] = React.useState(experienceContent[0].name);
   const [viewedExpColor, setViewedExpColor] = React.useState(experienceContent[0].color);
 
   const changeViewedExp = (event, newExp) => {
+    const index = experienceContent.findIndex(e => e.name === newExp);
+    setIndex(index);
     setViewedExp(newExp);
-    setViewedExpColor(experienceContent[experienceContent.findIndex(e => e.name === newExp)].color);
+    setViewedExpColor(experienceContent[index].color);
+  };
+
+  const handleChangeIndex = index => {
+    setIndex(index);
+    setViewedExp(experienceContent[index].name);
+    setViewedExpColor(experienceContent[index].color);
   };
   return (
     <section className='experience' id='experiences'>
@@ -67,69 +77,75 @@ const Experience = () => {
                 />
               ))}
             </TabList>
-            {experienceContent.map((ec, idx) => (
-              <TabPanel
-                key={ec.name}
-                value={ec.name}
-                index={idx}
-                className='experience-tab'
-                sx={{
-                  maxWidth: '100vw',
-                  backgroundColor: 'rgba(251,249,249, 0.5)',
-                  borderRadius: '20px',
-                }}
-              >
-                <div className='experience-header'>
-                  <img src={ec.logo} alt={`${ec.name}'s logo.`} />
-                  <div className='experience-header-text'>
-                    <Typography variant='h3' sx={{ fontWeight: 300 }}>
-                      {ec.role}{' '}
-                      <span className='accent-text' style={{ color: ec.color, fontWeight: 500 }}>
-                        @ <br />
-                        <Link href={ec.url} underline='hover' color={ec.color}>
-                          {ec.name}
-                        </Link>
-                      </span>
-                    </Typography>
-                    <Typography variant='h4'>
-                      {ec.location}{' '}
-                      <span className='experience-date'>
-                        (
-                        {ec.startDate.toLocaleString('default', {
-                          month: 'short',
-                          year: 'numeric',
-                        })}{' '}
-                        -{' '}
-                        {ec.endDate.toLocaleString('default', { month: 'short', year: 'numeric' })})
-                      </span>
-                    </Typography>
+            <SwipeableViews axis={'x'} index={index} onChangeIndex={handleChangeIndex}>
+              {experienceContent.map((ec, idx) => (
+                <TabPanel
+                  key={ec.name}
+                  value={ec.name}
+                  index={idx}
+                  className='experience-tab'
+                  sx={{
+                    maxWidth: '100vw',
+                    backgroundColor: 'rgba(251,249,249, 0.5)',
+                    borderRadius: '20px',
+                  }}
+                >
+                  <div className='experience-header'>
+                    <img src={ec.logo} alt={`${ec.name}'s logo.`} />
+                    <div className='experience-header-text'>
+                      <Typography variant='h3' sx={{ fontWeight: 300 }}>
+                        {ec.role}{' '}
+                        <span className='accent-text' style={{ color: ec.color, fontWeight: 500 }}>
+                          @ <br />
+                          <Link href={ec.url} underline='hover' color={ec.color}>
+                            {ec.name}
+                          </Link>
+                        </span>
+                      </Typography>
+                      <Typography variant='h4'>
+                        {ec.location}{' '}
+                        <span className='experience-date'>
+                          (
+                          {ec.startDate.toLocaleString('default', {
+                            month: 'short',
+                            year: 'numeric',
+                          })}{' '}
+                          -{' '}
+                          {ec.endDate.toLocaleString('default', {
+                            month: 'short',
+                            year: 'numeric',
+                          })}
+                          )
+                        </span>
+                      </Typography>
+                    </div>
                   </div>
-                </div>
-                <div className='experience-tools'>
-                  {ec.tools.map(t => (
-                    <Typography key={t} className='experience-tool' variant='body1'>
-                      {t}
-                    </Typography>
-                  ))}
-                </div>
-                <ul className={`experience-body ${ec.emojiClass}`}>
-                  {ec.body.map(b => (
-                    <li key={b}>
-                      <Typography variant='body1'>{b}</Typography>
-                    </li>
-                  ))}
-                </ul>
-                {ec.quote && (
-                  <div className='experience-quote'>
-                    <FormatQuoteIcon />
-                    <Typography variant='body1'>{ec.quote}</Typography>
-                    <Typography className='quote-author' variant='body2'>
-                      {ec.quoteWriterName}, {ec.quoteWriterPosition}
-                    </Typography>
+                  <div className='experience-tools'>
+                    {ec.tools.map(t => (
+                      <Typography key={t} className='experience-tool' variant='body1'>
+                        {t}
+                      </Typography>
+                    ))}
                   </div>
-                )}
-              </TabPanel>
-            ))}
+                  <ul className={`experience-body ${ec.emojiClass}`}>
+                    {ec.body.map(b => (
+                      <li key={b}>
+                        <Typography variant='body1'>{b}</Typography>
+                      </li>
+                    ))}
+                  </ul>
+                  {ec.quote && (
+                    <div className='experience-quote'>
+                      <FormatQuoteIcon />
+                      <Typography variant='body1'>{ec.quote}</Typography>
+                      <Typography className='quote-author' variant='body2'>
+                        {ec.quoteWriterName}, {ec.quoteWriterPosition}
+                      </Typography>
+                    </div>
+                  )}
+                </TabPanel>
+              ))}
+            </SwipeableViews>
           </TabContext>
         </Container>
       </Fade>
